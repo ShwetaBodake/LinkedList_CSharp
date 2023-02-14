22024e90a05ae7b4373fd9b79ebd4667f61e7553
Shweta Bodake <shwetabodake8108@gmail.com>
2023-02-14 13:31:24 +0530

    [Shweta] Added Linked list program

---
+++ LinkedList_CSharp/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LinkedList_CSharp
+{
+	public class Node
+	{
+
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
+
---
+++ LinkedList_CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkedList_CSharp
+{
+class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List ");
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.Add(56);
+            customLinkedList.Add(30);
+            customLinkedList.Add(70);
+            customLinkedList.Display();
+            Console.ReadLine();
+        }
+    }
+}
+
